a1c7c6c53649d10b29634084c726f4115d10c239
juststop <69150118+juststopp@users.noreply.github.com>
2021-10-27 11:02:48 +0200

    Delete dist/src/utils directory

--- dist/src/utils/Faster.d.ts
+++
@@ -1,8 +0,0 @@
-import Client from "../../main";
-declare class Faster {
-    client: typeof Client;
-    constructor(client: typeof Client);
-    clean(text: string): Promise<string>;
-    lang(lang: string | undefined): JSON;
-}
-export default Faster;
