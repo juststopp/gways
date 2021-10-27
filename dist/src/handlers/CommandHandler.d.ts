04481d8b8dfc707047d15d05008fc726378c2ae4
juststop <69150118+juststopp@users.noreply.github.com>
2021-10-27 11:02:54 +0200

    Delete dist/src directory

--- dist/src/handlers/CommandHandler.d.ts
+++
@@ -1,8 +0,0 @@
-import type Client from "../../main";
-import { CommandInteraction } from "discord.js";
-declare class CommandHandler {
-    client: typeof Client;
-    constructor(client: typeof Client);
-    handle(interaction: CommandInteraction): Promise<void>;
-}
-export default CommandHandler;
